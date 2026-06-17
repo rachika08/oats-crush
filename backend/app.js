5e8b4d4648c8b48de374c4dd145635cdf10f3d11
rachika08 <rachikasharma05@gmail.com>
2026-06-17 21:12:59 +0530

    app.js updates

--- backend/app.js
+++ backend/app.js
@@ -38,6 +38,8 @@ app.get('/',(req,res)=>{
 connectDB();
 cleanupPendingOrders();
 
-app.listen(process.env.PORT,()=>{
+const PORT=process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
     console.log("server listening");
 })
