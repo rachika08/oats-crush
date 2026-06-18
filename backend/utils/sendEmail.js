c5e6e5a62eebf349fd55500515650d1a4cc05fbc
rachika08 <rachikasharma05@gmail.com>
2026-06-18 09:36:31 +0530

    modified

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,15 +1,19 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = async (to, subject, html) => {
-
     console.log("EMAIL_USER =", process.env.EMAIL_USER);
     console.log("EMAIL_PASS =", process.env.EMAIL_PASS ? "Loaded" : "Missing");
 
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
+        },
+        tls: {
+            rejectUnauthorized: false,
         },
     });
 
