68b44038a361287d8888092874cb1557e55f01aa
rachika08 <rachikasharma05@gmail.com>
2026-07-02 10:18:08 +0530

    cors edit

--- backend/app.js
+++ backend/app.js
@@ -72,11 +72,11 @@ app.use(
 );
 
 app.use(express.urlencoded({extended:true}));
-// app.use(cors());
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true
+// }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
